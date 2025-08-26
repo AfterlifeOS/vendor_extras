3730c552484c53985c446963ae50e3a29a30e4da
muthafuka X <muthafuka21@gmail.com>
2025-08-26 14:53:49 +0000

    rework extras -also extract font, icon shape, icon pack, signal and wifi icon from evolutionX
    Change-Id: Ib78312d71cbe1a3f45f348c4f974b470277afd83
    Signed-off-by: muthafuka X <muthafuka21@gmail.com>

---
+++ afterlife.mk
@@ -0,0 +1,284 @@
+# Copyright (C) 2025 AfterlifeOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Gallery
+PRODUCT_PACKAGES += \
+    Glimpse
+
+# MC and VB
+ifneq ($(AFTERLIFE_GAPPS),true)
+PRODUCT_PACKAGES += \
+    MotoCalculator \
+    ViaBrowser
+endif
+
+# Clocks
+ifneq ($(AFTERLIFE_GAPPS),true)
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap
+endif
+
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontAccuratistOverlay \
+    FontAclonicaOverlay \
+    FontAmaranteOverlay \
+    FontBariolOverlay \
+    FontCagliostroOverlay \
+    FontCoconOverlay \
+    FontComfortaaOverlay \
+    FontComicSansOverlay \
+    FontCookieRunOverlay \
+    FontCooljazzOverlay \
+    FontCoolstoryOverlay \
+    FontExotwoOverlay \
+    FontEvoSansOverlay \
+    FontEvolveSansOverlay \
+    FontFifa2018Overlay \
+    FontGrandHotelOverlay \
+    FontGeneralSansOverlay \
+    FontGoogleSansFlexOverlay \
+    FontHarmonySansOverlay \
+    FontLatoOverlay \
+    FontLinotteOverlay \
+    FontNokiaPureOverlay \
+    FontNothingDotHeadlineOverlay \
+    FontNothingDotOverlay \
+    FontNunitoOverlay \
+    FontOneUISansOverlay \
+    FontOneplusSansOverlay \
+    FontOneplusSlateOverlay \
+    FontPoppinsSourceOverlay \
+    FontOswaldOverlay \
+    FontPlayOverlay \
+    FontQuandoOverlay \
+    FontRedressedOverlay \
+    FontReemKufiOverlay \
+    FontRobotoCondensedOverlay \
+    FontRookeryOverlay \
+    FontRubikOverlay \
+    FontSamsungOneOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
+    FontSansSerifOverlay \
+    FontSansSerifProOverlay \
+    FontSonySketchOverlay \
+    FontStoropiaOverlay \
+    FontSurferOverlay \
+    FontUbuntuOverlay \
+    FontVolteOverlay
+
+# Icon Packs
+PRODUCT_PACKAGES += \
+    IconPackAcherusAndroidOverlay \
+    IconPackAcherusLauncherOverlay \
+    IconPackAcherusSettingsOverlay \
+    IconPackAcherusSystemUIOverlay \
+    IconPackAuroraAndroidOverlay \
+    IconPackAuroraSystemUIOverlay \
+    IconPackCircularAndroidOverlay \
+    IconPackCircularLauncherOverlay \
+    IconPackCircularSettingsOverlay \
+    IconPackCircularSystemUIOverlay \
+    IconPackCircularThemePickerOverlay \
+    IconPackFilledAndroidOverlay \
+    IconPackFilledLauncherOverlay \
+    IconPackFilledSettingsOverlay \
+    IconPackFilledSystemUIOverlay \
+    IconPackFilledThemePickerOverlay \
+    IconPackGradiconAndroidOverlay \
+    IconPackGradiconSystemUIOverlay \
+    IconPackKaiAndroidOverlay \
+    IconPackKaiLauncherOverlay \
+    IconPackKaiSettingsOverlay \
+    IconPackKaiSystemUIOverlay \
+    IconPackKaiThemePickerOverlay \
+    IconPackLornAndroidOverlay \
+    IconPackLornSystemUIOverlay \
+    IconPackNostalgicAndroidOverlay \
+    IconPackNostalgicLauncherOverlay \
+    IconPackNostalgicSettingsOverlay \
+    IconPackNostalgicSystemUIOverlay \
+    IconPackNostalgicThemePickerOverlay \
+    IconPackOutlineAndroidOverlay \
+    IconPackOutlineLauncherOverlay \
+    IconPackOutlineSettingsOverlay \
+    IconPackOutlineSystemUIOverlay \
+    IconPackPUIAndroidOverlay \
+    IconPackPUILauncherOverlay \
+    IconPackPUISettingsOverlay \
+    IconPackPUISystemUIOverlay \
+    IconPackPUIThemePickerOverlay \
+    IconPackPlumpyAndroidOverlay \
+    IconPackPlumpySystemUIOverlay \
+    IconPackRoundedAndroidOverlay \
+    IconPackRoundedLauncherOverlay \
+    IconPackRoundedSettingsOverlay \
+    IconPackRoundedSystemUIOverlay \
+    IconPackRoundedThemePickerOverlay \
+    IconPackSamAndroidOverlay \
+    IconPackSamLauncherOverlay \
+    IconPackSamSettingsOverlay \
+    IconPackSamSystemUIOverlay \
+    IconPackSamThemePickerOverlay \
+    IconPackVictorAndroidOverlay \
+    IconPackVictorLauncherOverlay \
+    IconPackVictorSettingsOverlay \
+    IconPackVictorSystemUIOverlay \
+    IconPackVictorThemePickerOverlay \
+    IconPackXperiaAndroidOverlay \
+    IconPackXperiaSettingsOverlay \
+    IconPackXperiaSystemUIOverlay
+
+# Icon Shapes
+PRODUCT_PACKAGES += \
+    IconShapeArchOverlay \
+    IconShapeCloudyOverlay \
+    IconShapeCylinderOverlay \
+    IconShapeFlowerOverlay \
+    IconShapeFourSidedCookieOverlay \
+    IconShapeHeartOverlay \
+    IconShapeHexagonOverlay \
+    IconShapeIosOverlay \
+    IconShapeLeafOverlay \
+    IconShapeMeowOverlay \
+    IconShapePebbleOverlay \
+    IconShapeRoundedHexagonOverlay \
+    IconShapeRoundedRectOverlay \
+    IconShapeSamsungOverlay \
+    IconShapeScrollOverlay \
+    IconShapeSevenSidedCookieOverlay \
+    IconShapeStretchedOverlay \
+    IconShapeSquareOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeVesselOverlay
+
+# Signal Icons
+PRODUCT_PACKAGES += \
+    AiirOSignalOverlay \
+    AquariumSignalOverlay \
+    AuroraSignalOverlay \
+    BananaSignalOverlay \
+    BarsSignalOverlay \
+    BoldSignalOverlay \
+    ButterflySignalOverlay \
+    CapsuleSignalOverlay \
+    CircleSignalOverlay \
+    DaunSignalOverlay \
+    DecSignalOverlay \
+    DeepSignalOverlay \
+    DoraSignalOverlay \
+    DottedSignalOverlay \
+    EqualSignalOverlay \
+    FaintUISignalOverlay \
+    FanSignalOverlay \
+    ForlornSignalOverlay \
+    GlummySignalOverlay \
+    GradiconSignalOverlay \
+    HeartbeatSignalOverlay \
+    HollowSignalOverlay \
+    HuaweiSignalOverlay \
+    IOSSignalOverlay \
+    InsideSignalOverlay \
+    IosSignalOverlay \
+    JapaneseSignalOverlay \
+    KoalaSignalOverlay \
+    LineDotSignalOverlay \
+    LinealSignalOverlay \
+    LinearSignalOverlay \
+    MicroWaveSignalOverlay \
+    MiniSignalOverlay \
+    NinjaSignalOverlay \
+    NothingDotSignalOverlay \
+    NumberSignalOverlay \
+    OdinSignalOverlay \
+    PillsSignalOverlay \
+    PlumpySignalOverlay \
+    ROGSignalOverlay \
+    RelSignalOverlay \
+    RomanSignalOverlay \
+    RoundSignalOverlay \
+    RouterSignalOverlay \
+    ScaleSignalOverlay \
+    ScrollSignalOverlay \
+    SeaSignalOverlay \
+    SharpSignalOverlay \
+    SleekSignalOverlay \
+    SneakySignalOverlay \
+    SpiralSignalOverlay \
+    StackSignalOverlay \
+    StrokeSignalOverlay \
+    TowerSignalOverlay \
+    WaffleSignalOverlay \
+    WannuiSignalOverlay \
+    WavySignalOverlay \
+    WindowsSignalOverlay \
+    WindySignalOverlay \
+    WingSignalOverlay \
+    XperiaSignalOverlay \
+    ZigZagSignalOverlay
+
+# WiFi Icons
+PRODUCT_PACKAGES += \
+    a16Wifi \
+    AiirOWiFiOverlay \
+    AuroraWiFiOverlay \
+    BarsWiFiOverlay \
+    BoldWiFiOverlay \
+    CapsuleWiFiOverlay \
+    DoraWiFiOverlay \
+    FaintUIWiFiOverlay \
+    ForlornWiFiOverlay \
+    GlummyWiFiOverlay \
+    GradiconWiFiOverlay \
+    HollowWiFiOverlay \
+    InsideWiFiOverlay \
+    IosWiFiOverlay \
+    JapaneseWiFiOverlay \
+    KoalaWiFiOverlay \
+    LandscapeWiFiOverlay \
+    LineDotWiFiOverlay \
+    LinealWiFiOverlay \
+    LinearWiFiOverlay \
+    MicroWaveWiFiOverlay \
+    NothingDotWiFiOverlay \
+    NumberWiFiOverlay \
+    PlumpyWiFiOverlay \
+    RoundWiFiOverlay \
+    RouterWiFiOverlay \
+    ScaleWiFiOverlay \
+    SharpWiFiOverlay \
+    SneakyWiFiOverlay \
+    SpiralWiFiOverlay \
+    StrokeWiFiOverlay \
+    TowerWiFiOverlay \
+    WaffleWiFiOverlay \
+    WavyWiFiOverlay \
+    WeedWiFiOverlay \
+    WindyWiFiOverlay \
+    XperiaWiFiOverlay \
+    ZigZagWiFiOverlay
+
+# Include {Lato,Rubik} fonts
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/extras/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    $(call find-copy-subdir-files,*,vendor/extras/prebuilt/product/media/audio/ui,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
